f1c918f4bae3e4c7c365c8d150687f6a6ecec25c
Deiziane Silva <deiziane_ns@hotmail.com>
2021-05-21 17:23:17 -0300

    :wrench: fix typo

--- client/src/graphql/queries/getLandingPage.ts
+++ client/src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,11 @@
 const GET_LANDING_PAGE = /* GraphQL*/ `
-	landingPage {
-    logo{
-      alternativeText
-      url
+  query GET_LANDING_PAGE {
+    landingPage {
+      logo{
+        alternativeText
+        url
+      }
     }
-  }
 }`;
 
 export default GET_LANDING_PAGE;
